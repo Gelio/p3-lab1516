bf91fda9b89cc9a0a7cc0377a95f982e6b4d2ccc
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2016-12-07 17:51:43 +0100

    lab10: change custom delegates to generic C# ones

--- lab10/ArrayMagic.cs
+++ lab10/ArrayMagic.cs
@@ -4,16 +4,13 @@ namespace lab10a
 {
 	public static class ArrayMagic
 	{
-        public delegate void forEachDelegate<T>(T element);
-        public delegate T generateArrayDelegate<T>();
-        public delegate T transformArrayDelegate<T>(T element);
 	    /* Tu powstaną metody: ForEach, TransformArray, GenerateArray */
-        public static void ForEach<T>(T[] array, forEachDelegate<T> func) {
+        public static void ForEach<T>(T[] array, Action<T> func) {
             foreach (var element in array)
                 func(element);
         }
 
-        public static T[] GenerateArray<T>(generateArrayDelegate<T> generateFunc, int k)
+        public static T[] GenerateArray<T>(Func<T> generateFunc, int k)
         {
             T[] array = new T[k];
             for (int i = 0; i < k; i++)
@@ -21,7 +18,7 @@ namespace lab10a
             return array;
         }
 
-        public static T[] TransformArray<T>(T[] array, transformArrayDelegate<T> transformFunc)
+        public static T[] TransformArray<T>(T[] array, Func<T, T> transformFunc)
         {
             T[] transformedArray = new T[array.Length];
             for (int i = 0; i < array.Length; i++)
